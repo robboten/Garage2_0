d5ef95439141f7fcde764a4089d68265d4142d03
robboten <robboten@gmail.com>
2022-12-27 11:31:27 +0100

    VehiclesController

---
+++ Garage2_0/Data/GarageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage2_0.Models;
+
+namespace Garage2_0.Data
+{
+    public class GarageContext : DbContext
+    {
+        public GarageContext (DbContextOptions<GarageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage2_0.Models.Vehicle> Vehicle { get; set; } = default!;
+    }
+}
---
+++ Garage2_0/Migrations/20221227102352_init.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage20.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Color = table.Column<int>(type: "int", nullable: false),
+                    Brand = table.Column<int>(type: "int", nullable: false),
+                    VehicleType = table.Column<int>(type: "int", nullable: false),
+                    Wheels = table.Column<int>(type: "int", nullable: false),
+                    RegistrationNr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
