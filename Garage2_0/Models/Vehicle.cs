c9422479ce23fc853ada1123dd034de6a82161e9
Zxewl <noahferm@gmail.com>
2022-12-27 16:18:48 +0100

    Added Validation

--- Garage2_0/Models/Vehicle.cs
+++ Garage2_0/Models/Vehicle.cs
@@ -9,19 +9,16 @@ namespace Garage2_0.Models
         //namn.Inga minus värden på antal hjul osv.
         public int Id { get; set; }
 
-        [MaxLength(10)]
-        [MinLength(0)]
         public Color Color { get; set; }
 
-        [MaxLength(10)]
-        [MinLength(0)]
         public Brand Brand { get; set; } //
 
         public VehicleType VehicleType { get; set; } //buss bil cykel osv?
 
-        [Range(0,6)]
+        [Range(0,16)]
         public int Wheels { get; set; }
 
+        [Required]
         [MaxLength(8)]
         [MinLength(0)]
         public string RegistrationNr { get; set; } = string.Empty;
@@ -29,6 +26,8 @@ namespace Garage2_0.Models
         [MaxLength(10)]
         [MinLength(0)]
         public string Model { get; set; }
+
+        [Required]
         DateTime TimeOfArrival { get; set; }
     }
 }
